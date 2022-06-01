f1d3c4ad05d7bd1dfe50a58e6b13c5d28de53f0d
hitoshi082508 <hitoshi082508@gmail.com>
2022-06-01 16:33:20 +0900

    themeファイルやreset.css周りを作成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,15 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { theme } from 'src/styles/theme'
+import 'src/styles/globals.css'
+import 'src/styles/reset.css'
+
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
