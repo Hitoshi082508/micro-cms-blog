543ee064c6bc290b7a6ff1a24ddb384391c48492
hitoshi082508 <hitoshi082508@gmail.com>
2022-06-01 17:26:58 +0900

    microCMSの機能のみ実装

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Custom404: React.FC = () => {
+  return (
+    <main className="main">
+      <p>ページがありません。</p>
+    </main>
+  )
+}
+
+export const Custom404Page = styled(Custom404)``
+
+export default Custom404Page
---
+++ src/pages/[id].tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import styled from 'styled-components'
+import { client } from 'src/lib/client'
+import { Blog } from 'src/types'
+
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: 'blogs' })
+
+  const paths = data.contents.map((content) => `/${content.id}`)
+  return { paths, fallback: false }
+}
+
+export const getStaticProps = async (context) => {
+  const id: string = context.params.id
+  const data: Blog = await client.get({ endpoint: 'blogs', contentId: id })
+
+  return {
+    props: {
+      blog: data,
+    },
+  }
+}
+
+type Props = {
+  className?: string
+  blog: Blog
+}
+
+export const IndexIdBase: React.FC<Props> = ({ className, blog }) => {
+  return <div className={className}>{blog.id}</div>
+}
+
+export const IndexId = styled(IndexIdBase)`
+  font-size: 100px;
+  ${({ theme }) => theme.media.sp`
+    font-size: 20px;
+  `}
+`
+
+export default IndexId
